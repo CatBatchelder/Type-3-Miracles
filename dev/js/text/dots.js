7d6426e1c8c98192e1e3959746ea4d19a77475f7
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-11-14 16:16:32 -0500

    tryign to add dot

--- dev/js/text/dots.js
+++ dev/js/text/dots.js
@@ -5,11 +5,12 @@ gsap.registerPlugin(MotionPathPlugin);
 
 const dotsTL = gsap.timeline();
 
+//gsap.set("#moval1", {x: 387, y: 173});
 
 var dotsDrawSpeed = 1;
 
 export function dotsAnimation(){
-    dotsTL.from("#moval1",{ duration: dotsDrawSpeed, alpha:0, ease:"none", motionPath: { path: "#mpath1", align: "#mpath1" }});
+    dotsTL.from("#moval1",{ duration: dotsDrawSpeed, alpha:0, ease:"none", motionPath: { path: "#mpath1", align: "self", alignOrigin: [0.5, 0.5] }});
  
 
     return dotsTL;
