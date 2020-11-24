5f7c30f36f508011eea8a74690cdd8cabcb42a5f
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-11-24 13:33:21 -0500

    edits from class

--- dev/js/text/zoom.js
+++ dev/js/text/zoom.js
@@ -3,15 +3,16 @@ import {gsap} from "gsap";
 
 const zoomTL = gsap.timeline();
 
-gsap.set("#Page-1", {scale: 2});
+gsap.set("#Page-1", {scale: 3.5});
 
 //var textDrawSpeed = 1;
 
 export function zoomAnimation(){
-    zoomTL.to("#Page-1", {y: -100, duration: 4, ease: "none"})
-        .to("#Page-1", {y: -200, x:-200, duration: 3, ease: "none"})
+    zoomTL.to("#Page-1", {y: -200, duration: 3, ease: "none"})
+        .to("#Page-1", {y: -400, x:-200, duration: 3, scale: 2, ease: "none"})
         .to("#Page-1", {y: -400, x:-400, scale: 1.75, duration: 3, ease: "none"})
         .to("#Page-1", {y: 0, x:0, scale: 1, duration: 3})
+        .to("#text", {fill: "#000", duration: 1})
         .to("#Page-1", {scale: .01, alpha:0, duration: 4, transformOrigin: "center"}, "path")
         .to("#text", {fill: "#ff0000", duration: 2}, "path")
         .to("#paths", {stroke: "#ff0000", duration: 2}, "path")
